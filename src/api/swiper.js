50a2aabfadcadafe416c980199c43536fa8f9fb9
wey <149201216@qq.com>
2020-02-11 11:41:49 +0800

    HTTP API调用云存储删除文件

--- src/api/swiper.js
+++ src/api/swiper.js
@@ -1,9 +1,19 @@
 import request from '@/utils/request'
 const baseURL = 'http://localhost:3000'
 
+// 获取swiper列表
 export function fetchList() {
   return request({
     url: `${baseURL}/swiper/list`,
+    method: 'get'
+  })
+}
+
+// 删除
+export function del(params) {
+  return request({
+    params,
+    url: `${baseURL}/swiper/del`,
     method: 'get'
   })
 }
