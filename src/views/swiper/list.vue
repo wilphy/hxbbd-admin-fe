7e8a866114b73c94b3c73c3131bdba1351fa6ce6
wey <149201216@qq.com>
2020-02-11 11:17:46 +0800

    图片上传成功的优化

--- src/views/swiper/list.vue
+++ src/views/swiper/list.vue
@@ -2,7 +2,11 @@
   <div>
     <!-- 图片上传 -->
     <div class="filter-container">
-      <el-upload action="http://localhost:3000/swiper/upload">
+      <el-upload
+        action="http://localhost:3000/swiper/upload"
+        :on-success="uploadSuccess"
+        :show-file-list="false"
+      >
         <el-button size="small" type="primary">点击上传</el-button>
       </el-upload>
     </div>
@@ -44,6 +48,15 @@ export default {
         this.swiperList = res.data
         this.loading = false
       })
+    },
+    uploadSuccess(res) {
+      if (res.id_list.length > 0) {
+        this.$message({
+          message: '上传成功',
+          type: 'success'
+        })
+        this.getList()
+      }
     }
   }
 }
